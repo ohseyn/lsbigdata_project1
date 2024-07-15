6c198f3b1096ccac59d39f5a523a111be40b477f
ohseoyeon <streamoh1111@gmail.com>
2024-07-15 14:43:10 +0900

    2024.07.15 파이썬 4장

--- 파이썬 4장.py
+++ 파이썬 4장.py
@@ -2,9 +2,21 @@ import pandas as pd
 df = pd.DataFrame({'name': ['김지훈', '이유진', '박동현', '김민지'],
 'english': [90, 80, 60, 70], 'math': [50, 60, 100, 20]})
 df
-df['english']
+type(df) # pandas.core.frame.DataFrame
+df['english'] # 0    90
+              # 1    80
+              # 2    60
+              # 3    70
+              # Name: english, dtype: int64
+type(df['english']) # pandas.core.series.Series
 sum(df['english'])/4
 sum(df['math'])/4
+
+df_fruit = pd.DataFrame({'제품': ['사과', '딸기', '수박'], 
+                         '가격': [1800, 1500, 3000], '판매량': [24, 38, 13]})
+df_fruit
+sum(df_fruit['가격'])/len(df_fruit)
+sum(df_fruit['판매량'])/len(df_fruit)
 
 df_exam = pd.read_excel('C:/Doit_Python-main/Data/excel_exam.xlsx')
 df_exam
