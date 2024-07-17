3eaa8e01dcd9c4dce1b7017beb9251fe199fec5c
ohseoyeon <streamoh1111@gmail.com>
2024-07-17 16:10:36 +0900

    commit

--- 프로젝트2_240716.py
+++ 프로젝트2_240716.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 ## 데이터 전처리
 
 # 데이터 불러오기
-df = pd.read_csv("C:/Doit_Python-main/Data/발화요인에_대한_월별_화재발생현황.csv")
+df = pd.read_csv("C:/Data/발화요인에_대한_월별_화재발생현황.csv")
 df
 df.columns
 
@@ -41,36 +42,44 @@ data_2020 = data_2020.reset_index(drop=True)
 data_2021 = data_2021.reset_index(drop=True)
 data_2022 = data_2022.reset_index(drop=True)
 
+# year 변수 추가
+data_2020['year']=2020
+data_2021['year']=2021
+data_2022['year']=2022
+
+# 세로로 합치기
+data = pd.concat([data_2020, data_2021, data_2022])
+data
+
 # 변환할 열 목록
 columns_to_convert = ['계', '전기적요인', '기계적요인', '화학적요인', '가스누출', 
                       '교통사고', '부주의', '기타', '자연적요인', '방화', '방화의심', '미상']
 
 # 각 열에 대해 pd.to_numeric 적용
 for column in columns_to_convert:
-    data_2020[column] = pd.to_numeric(data_2020[column])
-    data_2021[column] = pd.to_numeric(data_2021[column])
-    data_2022[column] = pd.to_numeric(data_2022[column])
+    data[column] = pd.to_numeric(data[column])
     
-data_2020.info()
-data_2021.info()
-data_2022.info()
+data.info()
 
 # 파생 변수 만들기
-data_2020["계절"] = np.where(data_2020["항목"].isin(["3월", "4월", "5월"]), "봄", 
-                    np.where(data_2020["항목"].isin(["6월", "7월", "8월"]), "여름",
-                    np.where(data_2020["항목"].isin(["9월", "10월", "11월"]), "가을", "겨울")))
-                    
-data_2021["계절"] = np.where(data_2021["항목"].isin(["3월", "4월", "5월"]), "봄", 
-                    np.where(data_2021["항목"].isin(["6월", "7월", "8월"]), "여름",
-                    np.where(data_2021["항목"].isin(["9월", "10월", "11월"]), "가을", "겨울")))
-                    
-data_2022["계절"] = np.where(data_2022["항목"].isin(["3월", "4월", "5월"]), "봄", 
-                    np.where(data_2022["항목"].isin(["6월", "7월", "8월"]), "여름",
-                    np.where(data_2022["항목"].isin(["9월", "10월", "11월"]), "가을", "겨울")))
-                    
-season20 = data_2020.groupby('계절').agg(계절별화재=('계','sum'))
-season21 = data_2021.groupby('계절').agg(계절별화재=('계','sum'))
-season22 = data_2022.groupby('계절').agg(계절별화재=('계','sum'))
+data["계절"] = np.where(data["항목"].isin(["3월", "4월", "5월"]), "spring", 
+               np.where(data["항목"].isin(["6월", "7월", "8월"]), "summer",
+               np.where(data["항목"].isin(["9월", "10월", "11월"]), "fall", "winter")))
+               
+# 계절 순서 지정
+data['계절'] = pd.Categorical(data['계절'], categories=["spring", "summer", "fall", "winter"], 
+                              ordered=True)
+                              
+# 계절별 화재 발생횟수 데이터프레임 생성
+seasonal_data = data.groupby(["year", "계절"]).agg(계절별화재 = ("계", "sum"))
 
-# 그래프
+# 그래프 시각화
+plt.figure(figsize=(12, 6))
+sns.lineplot(data = seasonal_data, x = "계절", y = "계절별화재", hue='year', marker='o')
 
+plt.title('Seasonal Fire Incidents')
+plt.xlabel('Season')
+plt.ylabel('Number of Incidents')
+
+plt.show()
+plt.clf()
