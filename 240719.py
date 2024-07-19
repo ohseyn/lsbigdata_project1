7f4aa59d43aaab24c54d2d0943966107ee2bade9
ohseoyeon <streamoh1111@gmail.com>
2024-07-19 12:21:04 +0900

    240719 오전 수업

--- 240719.py
+++ 240719.py
@@ -7,17 +7,18 @@ sum(np.arange(4) * np.array([1, 2, 2, 1])/6)
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 예제 numpy 배열 생성
+# 1번
 x = np.random.rand(10000, 5).mean(axis=1)
-x = np.random.rand(50000).reshape(-1, 5).mean(axis=1)
-plt.hist(x, bins=30, alpha=0.7, color="blue")
-plt.grid(False)
-plt.show()
-plt.clf()
+# 2번
+x = np.random.rand(50000).reshape(-1, 5)
+# rand 함수가 가능한 이유는 기본으로 0과 1 사이 난수를 뽑아내기 때문 
+# 만약 0과 1 사이가 아닌 다른 값으로 할 거라면 * 를 이용하면 된다
+x = np.random.rand(50000).reshape(-1, 5)*30
+x_mean = x.mean(axis=1)
 
 # 히스토그램(빈도표) 그리기
 # bins(가로축 구간 개수, 폭), grid(경계)
-plt.hist(data, bins=4, alpha=0.7, color="blue")
+plt.hist(data, bins=30, alpha=0.7, color="blue")
 plt.title("Histogram of Numpy Vector")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
