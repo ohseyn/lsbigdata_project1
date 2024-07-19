83b477159f4d46fabb88a004f9ff297056f51ff0
ohseoyeon <streamoh1111@gmail.com>
2024-07-19 12:02:48 +0900

    240719 commit

--- 240719.py
+++ 240719.py
@@ -8,14 +8,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 예제 numpy 배열 생성
-dat = np.random.randn(1000)
+x = np.random.rand(10000, 5).mean(axis=1)
+x = np.random.rand(50000).reshape(-1, 5).mean(axis=1)
+plt.hist(x, bins=30, alpha=0.7, color="blue")
+plt.grid(False)
+plt.show()
+plt.clf()
 
-# 히스토그램 그리기
-
-plt.hist(data, bins=30, alpha=0.7, color="blue")
+# 히스토그램(빈도표) 그리기
+# bins(가로축 구간 개수, 폭), grid(경계)
+plt.hist(data, bins=4, alpha=0.7, color="blue")
 plt.title("Histogram of Numpy Vector")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
-plt.grid(True)
+plt.grid(False)
 plt.show()
 plt.clf()
