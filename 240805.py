f68ea2c86d22cbe6185e32e3885e66b3b3e56d9e
ohseoyeon <streamoh1111@gmail.com>
2024-08-05 21:07:04 +0900

    240805 복습 완!

--- 240805.py
+++ 240805.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from sklearn.linear_model import LinearRegression
 
+# 다항회귀분석
 df = pd.read_csv('train.csv')
 df_test = pd.read_csv('test.csv')
 sub_df=pd.read_csv("sample_submission.csv")
@@ -26,7 +27,7 @@ x.isna().sum()
 #    "MasVnrArea" : x["MasVnrArea"].mode()[0], # 시리즈로 나와서 찾아서 가져옴
 #    "GarageYrBlt" : x["MasVnrArea"].mean()
 #    }
-x = x.fillna(value=fill_values)
+#x = x.fillna(value=fill_values)
 
 x["LotFrontage"] = x["LotFrontage"].fillna(house_train["LotFrontage"].mean())
 x["MasVnrArea"] = x["MasVnrArea"].fillna(house_train["MasVnrArea"].mean())
@@ -53,3 +54,44 @@ test_x = test_x.fillna(test_x.mean()) # 변수별로 mean값을 내서 변수별
 pred_y = model.predict(test_x)
 sub["SalePrice"] = pred_y
 sub.to_csv("C:/Users/User/Documents/LSBigDataSchool/lsbigdata_project1/sample_submission10.csv", index=False)
+
+#===============================================
+from scipy.stats import norm
+
+# 직선의 방정식
+# y = 2x+3의 그래프를 그려보세요!
+a = 2
+b = 3
+
+x = np.linspace(0, 100, 400)
+y = a*x + b
+
+# np.random.seed(20240805)
+obs_x = np.random.choice(np.arange(100), 20)
+epsilon_i= norm.rvs(loc=0, scale=10, size=20) # 분산이 크면 오차가 커짐(신뢰도 저하)
+obs_y = 2*obs_x + 3 + epsilon_i
+
+model = LinearRegression()
+obs_x=obs_x.reshape(-1, 1)
+model.fit(obs_x, obs_y)
+
+slope = model.coef_[0]
+intercept = model.intercept_
+
+s_x = np.linspace(0, 100, 400)
+i_y = model.coef_[0] * x + model.intercept_
+
+plt.plot(s_x, i_y, color='red', label='LinearRegression')
+plt.plot(x, y, color="black")
+plt.scatter(obs_x, obs_y, color="blue", s=3)
+plt.xlim([0, 100])
+plt.ylim([0, 300])
+plt.show()
+plt.clf()
+
+!pip install statsmodels
+import statsmodels.api as sm
+
+obs_x = sm.add_constant(obs_x)
+model = sm.OLS(obs_y, obs_x).fit()
+print(model.summary()) # coef 열 -> const: b(절편) x1: a(기울기) (model 값과 동일)
